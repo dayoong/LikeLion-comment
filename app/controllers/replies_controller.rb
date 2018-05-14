db284cd8bfcc33eb22ddf4043e43af30ee75c121
EC2 Default User <ec2-user@ip-172-31-47-182.us-east-2.compute.internal>
2018-05-14 10:29:46 +0000

    Edit show.html for comment

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,7 +1,16 @@
 class RepliesController < ApplicationController
   def create
+    @reply = Reply.new(reply_params)
+    @reply.save
+    
+    redirect_to '/'
   end
   
   def destroy
+  end
+  
+  private
+  def reply_params
+      params.require(:reply).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
   resources :posts
+  
+  post '/replies/create' => 'replies#create'
 end
